8da778decf58d60cb2b5f7be18053ed3d9eadcc3
Hangjit Rai <hangjit.rai@adslot.com>
2018-07-23 16:42:02 +1000

    Change Package to AsyncPackage

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
 
 namespace OpenInGVim
 {
@@ -8,14 +10,16 @@ namespace OpenInGVim
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(Options), "VsVim", Vsix.Name, 101, 102, true, new string[0], ProvidesLocalizedCategoryName = false)]
     [Guid(PackageGuids.guidPackageString)]
-    public sealed class VSPackage : Package
+    public sealed class VSPackage : AsyncPackage
     {
-        protected override void Initialize()
+        protected override Task InitializeAsync(System.Threading.CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress)
         {
             var options = (Options)GetDialogPage(typeof(Options));
-
-            Logger.Initialize(this, Vsix.Name);
+            Logger.Initialize(ServiceProvider.GlobalProvider, Vsix.Name);
             OpenVimCommand.Initialize(this, options);
+
+            return Task.FromResult<object>(null);
         }
     }
 }
